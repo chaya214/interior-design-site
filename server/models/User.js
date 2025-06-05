2fd5364fb2dd283dbf558e9a64ff8c1f4041846e
chaya214 <chaya4610@gmail.com>
2025-06-05 14:28:30 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   email: String,
-//   password: String
-// });
-
-// module.exports = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
